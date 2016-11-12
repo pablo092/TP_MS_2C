bb17f85aca238bb8c730bb68ab84fb4b6c6ee2d2
TomasRS <tomas_rs@hotmail.com>
2016-11-12 12:44:45 -0300

    Agrego grafico de ambos canales del audio estereo.

--- codigoTP/inicioDeTransformada.m
+++ codigoTP/inicioDeTransformada.m
@@ -10,5 +10,14 @@ canal2 = audio(1:end,2);
 
 %Grafico el archivo de audio a procesar (sin dividir canales por ahora) 
 t=(1/Fs:1/Fs:length(audio)/Fs);
-plot(t,audio);
+
+figure;
+subplot(1,2,1);
+plot(t,canal1);
+title('Gráfico audio canal 1 sin procesar');
+grid on
+
+subplot(1,2,2);
+plot(t,canal2);
+title('Gráfico audio canal 2 sin procesar');
 grid on

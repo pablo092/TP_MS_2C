7926fc678c12400223d6d1ad5291df517ef972d9
TomasRS <tomas_rs@hotmail.com>
2016-11-12 12:33:34 -0300

    Agrego lectura de audio y con su grafico

--- codigoTP/inicioDeTransformada.m
+++ codigoTP/inicioDeTransformada.m
@@ -1,4 +1,14 @@
 clc
 clear
 
-[audio,Fs] = audioread('Audio-TPMatSup.wav');
+%Leo el archivo de audio
+[audio,Fs] = audioread('C:\Users\tomas\TP_MS_2C\Audio-TPMatSup.wav');
+
+%Separo en canales el audio estereo
+canal1 = audio(1:end,1);
+canal2 = audio(1:end,2);
+
+%Grafico el archivo de audio a procesar (sin dividir canales por ahora) 
+t=(1/Fs:1/Fs:length(audio)/Fs);
+plot(t,audio);
+grid on

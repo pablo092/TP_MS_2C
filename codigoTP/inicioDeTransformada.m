8637b7c24aeea179f204ecda4a036d7a3b8c1251
TomasRS <tomas_rs@hotmail.com>
2016-11-14 16:50:28 -0300

    La Antitransf no se si no funciona o tarda demasiado...

--- codigoTP/inicioDeTransformada.m
+++ codigoTP/inicioDeTransformada.m
@@ -8,25 +8,11 @@ clear
 canal1 = audio(1:end,1);
 canal2 = audio(1:end,2);
 
-%Grafico el archivo de audio a procesar 
-t=(1/Fs:1/Fs:length(audio)/Fs);
-
-figure;
-subplot(1,2,1);
-plot(t,canal1);
-title('Gráfico audio canal 1 sin procesar');
-grid on
-
-
-subplot(1,2,2);
-plot(t,canal2);
-title('Gráfico audio canal 2 sin procesar');
-grid on
-
 
 %---------------------------------------------------------------------
 
 %Aplicamos Transformada Z a la señal de entrada
+
 T1 = transformadaZ(canal1);
 T2 = transformadaZ(canal2);
 
@@ -48,6 +34,24 @@ fprintf('Voy a aplicar Antitransformada Z.\n');
 AT1=iztrans(F1);
 fprintf('Terminé la 1º antitransformada.\n');
 AT2=iztrans(F2);
-
+ 
 fprintf('Terminé de aplicar Antitransformada Z.\n\n');
 
+%---------------------------------------------------------------------
+
+%GRAFICOS
+
+%Grafico el archivo de audio sin procesar
+t=(1/Fs:1/Fs:length(audio)/Fs);
+
+figure;
+subplot(1,2,1);
+plot(t,canal1);
+title('Gráfico audio canal 1 sin procesar');
+grid on
+
+
+subplot(1,2,2);
+plot(t,canal2);
+title('Gráfico audio canal 2 sin procesar');
+grid on

61295683f7c06b085c14d94061629c3df9ab8986
TomasRS <tomas_rs@hotmail.com>
2016-11-17 21:46:14 -0300

    Arreglo TP. ¡FUNCIONA!

--- codigoTP/main.m
+++ codigoTP/main.m
@@ -3,8 +3,8 @@ clear
 
 %Leo el archivo de audio
 [audio,Fs] = audioread('../Audio-TPMatSup.wav');
-
 fprintf('Leí el audio.\n');
+
 %Separo en canales el audio estereo
 canal1 = audio(1:end,1);
 canal2 = audio(1:end,2);
@@ -12,63 +12,42 @@ fprintf('Separé el audio en 2 canales.\n\n');
 
 %---------------------------------------------------------------------
 
-%Aplicamos Transformada Z a la señal de entrada
+%Aplicamos el filtro FIR
+fprintf('Aplicamos el filtro FIR a cada canal.\n');
 
-T1 = transformadaZ(canal1);
-T2 = transformadaZ(canal2);
+numFiltro = [0, 1, 1.1];
+denFiltro = [1, 0, -0.1];
 
-fprintf('Terminé de aplicar Transformada Z a la señal de entrada.\n\n');
-
-
-%---------------------------------------------------------------------
-
-%Aplicamos el filtro FIR devolviendo x[z]*h[z]
-F1=filtroFIR(T1);
-F2=filtroFIR(T2);
+filtroCanal1 = filter(numFiltro, denFiltro, canal1);
+filtroCanal2 = filter(numFiltro, denFiltro, canal2);
 
 fprintf('Terminé de aplicar los filtro FIR.\n\n');
 
 %---------------------------------------------------------------------
 
-%Aplicamos Antitransformada Z
-fprintf('Voy a aplicar Antitransformada Z.\n');
-AT1=iztrans(F1);
-fprintf('Terminé la 1º antitransformada.\n');
-AT2=iztrans(F2);
- 
-fprintf('Terminé de aplicar Antitransformada Z.\n\n');
-
-%---------------------------------------------------------------------
-
-%Calculo los valores de la respuesta final Y[n]
-resultadoCanal1 = valoresParaGraficar(AT1,length(canal1));
-resultadoCanal2 = valoresParaGraficar(AT2,length(canal2));
-
-%---------------------------------------------------------------------
-
 %Hacemos los gráficos
-fprintf('Se procede a graficar.\n\n');
+fprintf('Procedemos a graficar.\n\n');
 
 t=(1/Fs:1/Fs:length(audio)/Fs);
 
 figure;
 subplot(2,2,1);
-plot(t,canal1);
+plot(t, canal1);
 title('Gráfico canal 1 sin procesar');
 grid on
 
 subplot(2,2,2);
-plot(t,canal2);
+plot(t, canal2);
 title('Gráfico canal 2 sin procesar');
 grid on
 
 subplot(2,2,3);
-plot(t,resultadoCanal1);
+plot(t, filtroCanal1);
 title('Gráfico canal 1 procesado');
 grid on
-
+ 
 subplot(2,2,4);
-plot(t,resultadoCanal2);
+plot(t, filtroCanal2);
 title('Gráfico canal 2 procesado');
 grid on
 
